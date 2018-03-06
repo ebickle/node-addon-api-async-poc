236b450e96a100dc7e0d563dbc0440331afd892c
Eric Bickle <wolf.code@outlook.com>
2018-03-06 14:37:32 -0800

    Added NAPI_THROW_IF_FAILED macro

--- napi-async-inl.h
+++ napi-async-inl.h
@@ -3,6 +3,32 @@
 
 namespace Napi {
 
+// Helpers to handle functions exposed from C++.
+namespace details {
+
+#ifdef NAPI_CPP_EXCEPTIONS
+
+// When C++ exceptions are enabled, Errors are thrown directly. There is no need
+// to return anything after the throw statement. The variadic parameter is an
+// optional return value that is ignored.
+#define NAPI_THROW_IF_FAILED(env, status, ...)           \
+  if ((status) != napi_ok) throw Error::New(env);
+
+#else // NAPI_CPP_EXCEPTIONS
+
+// When C++ exceptions are disabled, Errors are thrown as JavaScript exceptions,
+// which are pending until the callback returns to JS.  The variadic parameter
+// is an optional return value; usually it is an empty result.
+#define NAPI_THROW_IF_FAILED(env, status, ...)           \
+  if ((status) != napi_ok) {                             \
+    Error::New(env).ThrowAsJavaScriptException();        \
+    return __VA_ARGS__;                                  \
+  }
+
+#endif // NAPI_CPP_EXCEPTIONS
+
+} // namespace details
+
 ////////////////////////////////////////////////////////////////////////////////
 // AsyncWork class
 ////////////////////////////////////////////////////////////////////////////////
@@ -18,8 +44,7 @@ inline AsyncWork::AsyncWork(Napi::Env env,
 
   napi_value resource_id;
   napi_status status = napi_create_string_latin1(
-    env, "generic", NAPI_AUTO_LENGTH, &resource_id);
-  
+    env, "generic", NAPI_AUTO_LENGTH, &resource_id);  
   NAPI_THROW_IF_FAILED(env, status);
 
   napi_async_work work;
